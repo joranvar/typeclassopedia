1aab6af528222e013ee8c162adb687f73c11553d
joranvar <bart.post@gmail.com>
2016-04-16 11:51:51 +0200

    3.2.1 Implement Functor' (Either e)

---
+++ src/Main.hs
@@ -0,0 +1,13 @@
+import Prelude (IO, undefined)
+
+class Functor' f where
+  fmap :: (a -> b) -> f a -> f b
+
+data Either e a = Left e | Right a
+
+instance Functor' (Either e) where
+  fmap _ (Left e)  = Left e
+  fmap g (Right a) = Right (g a)
+
+main :: IO ()
+main = undefined
