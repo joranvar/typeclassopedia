e03c98d146330ebb1c2eb63697b5e40790d764c8
joranvar <bart.post@gmail.com>
2016-04-16 12:24:17 +0200

    3.2.1 Implement Functor' ((->) e)

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-import Prelude (IO, undefined)
+import Prelude (IO, (.), undefined)
 
 class Functor' f where
   fmap :: (a -> b) -> f a -> f b
@@ -8,6 +8,9 @@ data Either e a = Left e | Right a
 instance Functor' (Either e) where
   fmap _ (Left e)  = Left e
   fmap g (Right a) = Right (g a)
+
+instance Functor' ((->) e) where
+  fmap = (.)
 
 main :: IO ()
 main = undefined
